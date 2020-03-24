1b8f59aa84e2d7c0da3b0701245521e966b93c10
Tito Dal Canton <tito@dalcanton.it>
2020-03-24 21:00:27 +0100

    Use new data file

--- get_data.sh
+++ get_data.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+curl -o raw.txt https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
 
 # extract header
-head -n 1 time_series_19-covid-Confirmed.csv | tr ',' '\n' > header.txt
+head -n 1 raw.txt | tr ',' '\n' > header.txt
 
 # extract counts for Italy
-grep Italy time_series_19-covid-Confirmed.csv | tr ',' '\n' > bar.txt
+grep '^,Italy' raw.txt | tr ',' '\n' > bar.txt
 
 # make a nice count-vs-time file
 paste header.txt bar.txt | grep '/20' | tr '/' ' ' | tr -d -C '[0-9] \t\n' > count_vs_time_it.txt
 
 # extract counts for France
-grep France time_series_19-covid-Confirmed.csv | tr ',' '\n' > bar.txt
+grep '^,France' raw.txt | tr ',' '\n' > bar.txt
 
 # make a nice count-vs-time file
 paste header.txt bar.txt | grep '/20' | tr '/' ' ' | tr -d -C '[0-9] \t\n' > count_vs_time_fr.txt
